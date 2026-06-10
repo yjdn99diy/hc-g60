b965f1456b149c3216e278a61ec76a5373f65093
初学者 <62415940+yjdn99diy@users.noreply.github.com>
2026-06-10 08:45:35 +0000

    Update mt7981.mk

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -809,3 +809,20 @@ define Device/routerich_ax3000
   DEVICE_PACKAGES := $(MT7981_USB_PKGS)
 endef
 TARGET_DEVICES += routerich_ax3000
+
+define Device/hc_g60
+  DEVICE_VENDOR := HC
+  DEVICE_MODEL := G60
+  DEVICE_DTS := mt7981-hc-g60
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  SUPPORTED_DEVICES := hc,g60
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 114816k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += hc_g60
